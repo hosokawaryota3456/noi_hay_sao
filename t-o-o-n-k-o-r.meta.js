16b6cb984f0061216b73286e1fcc9af7952209ad
hosokawaryota3456 <hosokawaryota3456@github.com>
2024-10-17 15:50:06 +0700

    init

---
+++ t-o-o-n-k-o-r.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         t-o-o-n-k-o-r
+// @namespace    t-o-o-n-k-o-r.user.js
+// @version      2024.10.17
+// @description  down t-o-o-n-k-o-r
+// @author       You
+// @include      /^https?://toonkor\d+\.com/.+?\.html/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=toonkor446.com
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
+// @downloadURL  https://github.com/hosokawaryota3456/noi_hay_sao/raw/main/t-o-o-n-k-o-r.user.js
+// @updateURL    https://github.com/hosokawaryota3456/noi_hay_sao/raw/main/t-o-o-n-k-o-r.meta.js
+// ==/UserScript==
